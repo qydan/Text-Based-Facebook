08d1ebf8ee942d4d59466147d7c9248aa4b17caf
Aydan Eng <94135697+qydan@users.noreply.github.com>
2024-12-27 18:13:16 -0500

    Add files via upload

---
+++ a2_nodes.h
@@ -0,0 +1,36 @@
+/********* nodes.h ********
+    Student Name 	= Aydan Eng
+    Student Number	= 101298520
+*/
+
+#ifndef __A2_NODES_H__
+#define __A2_NODES_H__
+
+/********** DON'T MODIFY **********/
+// Structure to represent a linked list of users
+typedef struct user
+{
+    char username[30];
+    char password[15];
+    struct friend *friends;
+    struct post *posts;
+    struct user *next;
+} user_t;
+
+// Structure to represent a linked list of a user's friends
+typedef struct friend
+{
+    char username[30];
+    struct post **posts;
+    struct friend *next;
+}
+friend_t;
+
+// Structure to represent a linked list of a user's posts
+typedef struct post
+{
+    char content[250];
+    struct post *next;
+} post_t;
+
+#endif
